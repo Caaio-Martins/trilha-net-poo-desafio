2ea682fd6ca4736a854dbae24183b49eb14a8bd2
Caaio-Martins <caio.antnmartins@gmail.com>
2023-12-29 17:13:23 -0300

    Implementações em 29/12/23

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string Imei { get; set; }
+
+        private int Memoria { get; set; }
+        
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
